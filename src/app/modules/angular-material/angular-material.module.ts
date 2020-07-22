98e9fa9914b08470a9bfd58872014ee2368f7d57
sulbh-06 <sulbha06@gmail.com>
2020-07-23 00:11:08 +0530

    productsearch design change

--- src/app/modules/angular-material/angular-material.module.ts
+++ src/app/modules/angular-material/angular-material.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
-import {MatButtonToggleModule, MatTableModule, MatFormFieldModule, MatSelectModule, MatIconModule, MatPaginatorModule, MatSortModule} from '@angular/material';
+import {MatButtonToggleModule, MatFormFieldModule, MatSelectModule, MatIconModule, MatPaginatorModule, MatSortModule} from '@angular/material';
 import {MatInputModule} from '@angular/material/input';
 import {MatTooltipModule} from '@angular/material/tooltip';
 import {MatButtonModule} from '@angular/material/button';    
 import {MatDatepickerModule} from '@angular/material/datepicker';
 import {MatCheckboxModule} from '@angular/material/checkbox';
 import {MatTreeModule} from '@angular/material/tree';
+import {MatTableModule} from '@angular/material/table';
 const Material = [
   MatTooltipModule,
   MatButtonModule,
